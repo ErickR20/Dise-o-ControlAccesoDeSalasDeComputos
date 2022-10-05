f5effaf8332bf14028da3bdea7a3be22b9e609b6
Erick Roberto <Erick Roberto@DESKTOP-307N2HB>
2022-10-05 11:11:56 -0500

    Agregar archivos de proyecto.

---
+++ Diseño-ControlAccesoDeSalasDeComputos/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Diseño_ControlAccesoDeSalasDeComputos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iNICIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salasDeComputoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hORARIOSDETRABAJOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mATERIASIMPARTIDASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salaDeComputo1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salaDeComputo2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salaDeComputo3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.telecomunicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estructuraDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallerDeBasesDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sistemasOperativosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diseñoEImpDeBdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(169, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iNICIOToolStripMenuItem,
+            this.salasDeComputoToolStripMenuItem,
+            this.hORARIOSDETRABAJOToolStripMenuItem,
+            this.mATERIASIMPARTIDASToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iNICIOToolStripMenuItem
+            // 
+            this.iNICIOToolStripMenuItem.Name = "iNICIOToolStripMenuItem";
+            this.iNICIOToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.iNICIOToolStripMenuItem.Text = "INICIO";
+            // 
+            // salasDeComputoToolStripMenuItem
+            // 
+            this.salasDeComputoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salaDeComputo1ToolStripMenuItem,
+            this.salaDeComputo2ToolStripMenuItem,
+            this.salaDeComputo3ToolStripMenuItem});
+            this.salasDeComputoToolStripMenuItem.Name = "salasDeComputoToolStripMenuItem";
+            this.salasDeComputoToolStripMenuItem.Size = new System.Drawing.Size(130, 20);
+            this.salasDeComputoToolStripMenuItem.Text = "SALAS DE COMPUTO";
+            this.salasDeComputoToolStripMenuItem.Click += new System.EventHandler(this.salasDeComputoToolStripMenuItem_Click);
+            // 
+            // hORARIOSDETRABAJOToolStripMenuItem
+            // 
+            this.hORARIOSDETRABAJOToolStripMenuItem.Name = "hORARIOSDETRABAJOToolStripMenuItem";
+            this.hORARIOSDETRABAJOToolStripMenuItem.Size = new System.Drawing.Size(147, 20);
+            this.hORARIOSDETRABAJOToolStripMenuItem.Text = "HORARIOS DE TRABAJO";
+            // 
+            // mATERIASIMPARTIDASToolStripMenuItem
+            // 
+            this.mATERIASIMPARTIDASToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.telecomunicacionesToolStripMenuItem,
+            this.estructuraDeDatosToolStripMenuItem,
+            this.tallerDeBasesDeDatosToolStripMenuItem,
+            this.sistemasOperativosToolStripMenuItem,
+            this.diseñoEImpDeBdToolStripMenuItem});
+            this.mATERIASIMPARTIDASToolStripMenuItem.Name = "mATERIASIMPARTIDASToolStripMenuItem";
+            this.mATERIASIMPARTIDASToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.mATERIASIMPARTIDASToolStripMenuItem.Text = "MATERIAS IMPARTIDAS";
+            // 
+            // salaDeComputo1ToolStripMenuItem
+            // 
+            this.salaDeComputo1ToolStripMenuItem.Name = "salaDeComputo1ToolStripMenuItem";
+            this.salaDeComputo1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salaDeComputo1ToolStripMenuItem.Text = "Sala de computo 1";
+            // 
+            // salaDeComputo2ToolStripMenuItem
+            // 
+            this.salaDeComputo2ToolStripMenuItem.Name = "salaDeComputo2ToolStripMenuItem";
+            this.salaDeComputo2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salaDeComputo2ToolStripMenuItem.Text = "Sala de computo 2";
+            // 
+            // salaDeComputo3ToolStripMenuItem
+            // 
+            this.salaDeComputo3ToolStripMenuItem.Name = "salaDeComputo3ToolStripMenuItem";
+            this.salaDeComputo3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salaDeComputo3ToolStripMenuItem.Text = "Sala de computo 3";
+            // 
+            // telecomunicacionesToolStripMenuItem
+            // 
+            this.telecomunicacionesToolStripMenuItem.Name = "telecomunicacionesToolStripMenuItem";
+            this.telecomunicacionesToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.telecomunicacionesToolStripMenuItem.Text = "Telecomunicaciones";
+            // 
+            // estructuraDeDatosToolStripMenuItem
+            // 
+            this.estructuraDeDatosToolStripMenuItem.Name = "estructuraDeDatosToolStripMenuItem";
+            this.estructuraDeDatosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.estructuraDeDatosToolStripMenuItem.Text = "Estructura de datos";
+            // 
+            // tallerDeBasesDeDatosToolStripMenuItem
+            // 
+            this.tallerDeBasesDeDatosToolStripMenuItem.Name = "tallerDeBasesDeDatosToolStripMenuItem";
+            this.tallerDeBasesDeDatosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.tallerDeBasesDeDatosToolStripMenuItem.Text = "Taller de bases de datos";
+            // 
+            // sistemasOperativosToolStripMenuItem
+            // 
+            this.sistemasOperativosToolStripMenuItem.Name = "sistemasOperativosToolStripMenuItem";
+            this.sistemasOperativosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.sistemasOperativosToolStripMenuItem.Text = "Sistemas operativos";
+            // 
+            // diseñoEImpDeBdToolStripMenuItem
+            // 
+            this.diseñoEImpDeBdToolStripMenuItem.Name = "diseñoEImpDeBdToolStripMenuItem";
+            this.diseñoEImpDeBdToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.diseñoEImpDeBdToolStripMenuItem.Text = "Diseño e imp. de bd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem iNICIOToolStripMenuItem;
+        private ToolStripMenuItem salasDeComputoToolStripMenuItem;
+        private ToolStripMenuItem salaDeComputo1ToolStripMenuItem;
+        private ToolStripMenuItem salaDeComputo2ToolStripMenuItem;
+        private ToolStripMenuItem salaDeComputo3ToolStripMenuItem;
+        private ToolStripMenuItem hORARIOSDETRABAJOToolStripMenuItem;
+        private ToolStripMenuItem mATERIASIMPARTIDASToolStripMenuItem;
+        private ToolStripMenuItem telecomunicacionesToolStripMenuItem;
+        private ToolStripMenuItem estructuraDeDatosToolStripMenuItem;
+        private ToolStripMenuItem tallerDeBasesDeDatosToolStripMenuItem;
+        private ToolStripMenuItem sistemasOperativosToolStripMenuItem;
+        private ToolStripMenuItem diseñoEImpDeBdToolStripMenuItem;
+    }
+}
---
+++ Diseño-ControlAccesoDeSalasDeComputos/Form1.cs
@@ -0,0 +1,15 @@
+namespace Diseño_ControlAccesoDeSalasDeComputos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void salasDeComputoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
